122fa13c641dda6d5af884712cae33bf03de1b97
Pranav Sharma <pranavss3468@gmail.com>
2023-03-30 19:19:33 +0530

    Splash Activity

---
+++ app/src/main/java/com/raviandroid/she/SplashActivity.java
@@ -0,0 +1,92 @@
+package com.raviandroid.she;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        TextView typingt = findViewById(R.id.textView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.setText("H");
+            }
+        }, 300);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("e");
+            }
+        }, 400);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("r");
+            }
+        }, 500);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("S");
+            }
+        }, 600);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("h");
+            }
+        }, 700);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("i");
+            }
+        }, 800);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("e");
+            }
+        }, 900);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("l");
+            }
+        }, 1000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                typingt.append("d");
+            }
+        }, 1100);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1500);
+    }
+
+
+}
+
